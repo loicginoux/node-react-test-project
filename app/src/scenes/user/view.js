e9847b15e4346579eb1f310a48dc3ba30aaca6ba
Loic <lginoux@gmail.com>
2024-03-11 10:46:00 +0100

    feat: add user montly activity view
    allow have a quick look at a user current worload by displaying statistics for current month.
    How many hours worked this month, how much this user cost this month and how much it has billed. Statistics are per project and in total.

--- app/src/scenes/user/view.js
+++ app/src/scenes/user/view.js
@@ -1,11 +1,7 @@
-import { Formik } from "formik";
 import React, { useEffect, useState } from "react";
-import toast from "react-hot-toast";
-import { useHistory, useParams } from "react-router-dom";
-
 import Loader from "../../components/loader";
-import LoadingButton from "../../components/loadingButton";
 import api from "../../services/api";
+import { useHistory, useParams } from "react-router-dom";
 
 export default () => {
   const [user, setUser] = useState(null);
@@ -31,120 +27,137 @@ export default () => {
 const Detail = ({ user }) => {
   const history = useHistory();
 
-  async function deleteData() {
-    const confirm = window.confirm("Are you sure ?");
-    if (!confirm) return;
-    await api.remove(`/user/${user._id}`);
-    toast.success("successfully removed!");
-    history.push(`/user`);
-  }
-
   return (
-    <Formik
-      initialValues={user}
-      onSubmit={async (values) => {
-        try {
-          await api.put(`/user/${user._id}`, values);
-          toast.success("Updated!");
-        } catch (e) {
-          console.log(e);
-          toast.error("Some Error!");
-        }
-      }}>
-      {({ values, handleChange, handleSubmit, isSubmitting }) => {
-        return (
-          <React.Fragment>
-            <div className="flex justify-between flex-wrap mt-4">
-              <div className="w-full md:w-[260px] mt-[10px] md:mt-0 ">
-                <div className="text-[14px] text-[#212325] font-medium	">Name</div>
-                <input
-                  className="projectsInput text-[14px] font-normal text-[#212325] bg-[#F9FBFD] rounded-[10px]"
-                  name="name"
-                  disabled
-                  value={values.name}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="w-full md:w-[260px] mt-[10px] md:mt-0">
-                <div className="text-[14px] text-[#212325] font-medium	">Email</div>
-                <input className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]" name="email" value={values.email} onChange={handleChange} />
-              </div>
-              <div className="w-full md:w-[165px] mt-[10px] md:mt-0">
-                <div className="text-[14px] text-[#212325] font-medium	">Status</div>
-                <select className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]" type="select" name="status" value={values.status} onChange={handleChange}>
-                  <option value="active">Active</option>
-                  <option value="inactive">Inactive</option>
-                </select>
-              </div>
+    <React.Fragment>
+      <div className="pl-20 pt-24 pb-4 w-[98%]">
+        <div className="bg-[#FFFFFF] border border-[#E5EAEF] py-3 rounded-[16px]">
+          <div className="flex justify-between px-3 pb-2  border-b border-[#E5EAEF]">
+            <div>
+              <span className="text-[18px] text-[#212325] font-semibold">User details</span>
             </div>
-            <div className="flex flex-wrap justify-between mt-4	space-x-3">
-              <div className="w-full md:w-[260px] ">
-                <div className="text-[14px] text-[#212325] font-medium">Job title</div>
-                <input
-                  className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px] bg-[#fff]"
-                  name="job_title"
-                  value={values.job_title}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-wrap justify-between mt-4">
-              <div className="w-full md:w-[260px] ">
-                <div className="text-[14px] text-[#212325] font-medium	">Days worked</div>
-                <input
-                  className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]"
-                  type="number"
-                  name="days_worked"
-                  value={values.days_worked}
-                  onChange={handleChange}
-                />{" "}
-              </div>
-              <div className="w-full md:w-[260px] ">
-                <div className="text-[14px] text-[#212325] font-medium	">Cost per day</div>
-                <input
-                  className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]"
-                  type="number"
-                  name="costPerDay"
-                  value={values.costPerDay}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="w-full md:w-[260px] ">
-                <div className="text-[14px] text-[#212325] font-medium	">Sell per day</div>
-                <input
-                  className="projectsInput text-[14px] font-normal text-[#212325] rounded-[10px]"
-                  type="number"
-                  name="sellPerDay"
-                  value={values.sellPerDay}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <div className="w-full mt-3">
-              <div className="text-[14px] text-[#212325] font-medium	">Description</div>
-              <textarea
-                className="w-full text-[14px] font-normal text-[#212325] border border-[#ced4da] mt-2 rounded-[10px] text-sm p-2  focus:outline-none focus:ring focus:ring-[#80bdff]"
-                rows="12"
-                name="description"
-                value={values.description}
-                onChange={handleChange}></textarea>
-            </div>
-
-            <div className="flex  mt-2">
-              <LoadingButton
-                className="bg-[#0560FD] text-[16px] font-medium text-[#FFFFFF] py-[12px] px-[22px] rounded-[10px]"
-                loading={isSubmitting}
-                onClick={handleSubmit}>
-                Update
-              </LoadingButton>
-              <button className="ml-[10px] bg-[#F43F5E] text-[16px] font-medium text-[#FFFFFF] py-[12px] px-[22px] rounded-[10px]" onClick={deleteData}>
-                Delete
+            <div className="flex items-center gap-2">
+              <button
+                onClick={() => history.push(`/user/edit/${user?._id}`)}
+                className="border !border-[#0560FD] text-[#0560FD] py-[7px] px-[20px] bg-[#FFFFFF] rounded-[16px]">
+                Edit
               </button>
             </div>
-          </React.Fragment>
-        );
-      }}
-    </Formik>
+          </div>
+          <UserMonthActivity user={user}/>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
+
+const UserMonthActivity = ({ user }) => {
+  const [projectActivities, setProjectActivities] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let date = new Date();
+      const dateFrom = new Date(date.getFullYear(), date.getMonth(), 1)
+      const dateTo = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+      const { data } = await api.get(`/activity?dateFrom=${dateFrom.getTime()}&dateTo=${dateTo.getTime()}&userId=${encodeURIComponent(user._id)}`);
+      if (data.length == 0) return;
+      const projects = await api.get(`/project/list`);
+      setProjectActivities(projectAggregatedInfos(data, projects.data, user))
+    })();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex flex-wrap p-3">
+        <div className="w-full ">
+          <div className="flex gap-3">
+            <div className="w-full">
+              <div className="flex justify-between gap-2">
+                <div className="flex gap-20">
+                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">Activity this month</span>
+                </div>
+              </div>
+              <div className="w-full md:w-[50%]">
+                <div className="pt-2 ">
+                  <span className="text-[16px] text-[#676D7C] font-bold">Name: {user.name.toString()}</span>
+                </div>
+                <div className="pt-2 ">
+                  <span className="text-[16px] text-[#676D7C] font-medium">Cost per day: {user.costPerDay ? user.costPerDay : ""}€</span>
+                </div>
+                <div className="pt-2 ">
+                  <span className="text-[16px] text-[#676D7C] font-medium">Sell per day: {user.sellPerDay ? user.sellPerDay : ""}€</span>
+                </div>
+                <div className="pt-6 ">
+                  <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                          <tr>
+                              <th scope="col" className="px-6 py-3">
+                                  Project
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  Days worked
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  Cost
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  Billed
+                              </th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          {projectActivities.map((project, i) => (
+                            <tr key={i} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th
+                                  scope="row"
+                                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                  {project.projectName}
+                                </th>
+                                <td className="px-6 py-4">{project.daysWorked}</td>
+                                <td className="px-6 py-4">{project.cost}€</td>
+                                <td className="px-6 py-4">{project.billed}€</td>
+                            </tr>
+                          ))}
+                          <tr className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                              <th scope="col" className="px-6 py-3">
+                                  Total
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  {projectActivities.reduce((acc, b) => acc + b.daysWorked, 0)}
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  {projectActivities.reduce((acc, b) => acc + b.cost, 0)}€
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  {projectActivities.reduce((acc, b) => acc + b.billed, 0)}€
+                              </th>
+                          </tr>
+                      </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap p-3 gap-4"></div>
+    </div>
+  );
+};
+
+
+const projectAggregatedInfos = (activities, projects, user) => {
+  const hoursPerDay = 8;
+
+  return projects.map((project) => {
+    const projectwithActivity = activities.find((activity) => activity.projectId === project._id);
+    if (!projectwithActivity) return;
+    const hoursWorked = projectwithActivity.detail.reduce((acc, b) => acc + b.value, 0)
+    const daysWorked = hoursWorked / hoursPerDay;
+    return {
+      projectName: project.name,
+      daysWorked: daysWorked,
+      cost: daysWorked * user.costPerDay,
+      billed: daysWorked * user.sellPerDay,
+    }
+  }).filter(n => n)
+}
